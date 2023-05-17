f492b0148b9b2c73199c5389ca6e9b7af74dd4b3
XavierQuerol <101579003+XavierQuerol@users.noreply.github.com>
2023-05-17 17:36:47 +0200

    add transformation

--- PHOC/dataset.py
+++ PHOC/dataset.py
@@ -1,23 +1,28 @@
 from torch.utils.data import Dataset
-import cv2
 from torchvision.io import read_image
 
 from PHOC import phoc
 
 class dataset(Dataset):
     def __init__(self, annotations_file, img_dir, transform = None):
+
         with open(annotations_file, "r") as file:
             self.paths = file.readlines()
         self.img_dir = img_dir
         self.transform = transform
 
     def __len__(self):
+
         return len(self.paths)
     
     def __getitem__(self, idx):
+
         path = self.img_dir + self.paths[idx].split(" ")[0][1:]
         img = read_image(path)
         target = phoc(self.paths[idx].split("_")[1])
+
+        if self.transform != None:
+            img = self.transform(img)
         return img, target
     
 images_dataset = dataset("C:/Users/xavid/Desktop/dataset/mjsynth/mnt/ramdisk/max/90kDICT32px/annotation_val.txt",
