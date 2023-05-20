a9416e459284737bc098570b45e29c9c6eaa2ab5
XavierQuerol <101579003+XavierQuerol@users.noreply.github.com>
2023-05-20 15:04:55 +0200

    bbox to utils

--- utils/utils.py
+++ utils/utils.py
@@ -50,3 +50,21 @@ def make(config, device="cuda"):
         model.parameters(), lr=config.learning_rate)
     
     return model, train_loader, test_loader, criterion, optimizer
+
+def convert_bbox_to_yolo(bbox, image_width, image_height):
+    x, y, width, height = bbox
+
+    # Calculate the bounding box center coordinates
+    center_x = x + width / 2
+    center_y = y + height / 2
+
+    # Normalize the coordinates by dividing them by the image width and height
+    yolo_center_x = center_x / image_width
+    yolo_center_y = center_y / image_height
+
+    # Normalize the width and height by dividing them by the image width and height
+    yolo_width = width / image_width
+    yolo_height = height / image_height
+
+    # Return the bounding box in YOLO format
+    return yolo_center_x, yolo_center_y, yolo_width, yolo_height
