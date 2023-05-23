d85f6e2bed498e8b7557bd0c48d0effa67a6b44b
XavierQuerol <101579003+XavierQuerol@users.noreply.github.com>
2023-05-23 10:42:40 +0200

    knn with all lexicon

--- PHOC_UNET/utils/create_knn_classifier.py
+++ PHOC_UNET/utils/create_knn_classifier.py
@@ -1,4 +1,4 @@
-from utils import phoc
+from build_phoc import phoc
 from sklearn.neighbors import KNeighborsClassifier
 
 import pickle
@@ -9,13 +9,13 @@ annotations_file = "Datasets/lexicon.txt"
 with open(annotations_file, "r") as file:
     list_of_words = file.readlines()
 
-list_of_words = [l[:-1] for l in list_of_words][:40]
+list_of_words = [l[:-1] for l in list_of_words]
 phoc_representations = phoc(list_of_words)
 
 knn = KNeighborsClassifier(n_neighbors=1)
 knn.fit(phoc_representations, list_of_words)
 
-knnPickle = open('PHOC/knn_classifier', 'wb') 
+knnPickle = open('PHOC_UNET/utils/knn_classifier', 'wb') 
 pickle.dump(knn, knnPickle)  
 knnPickle.close()
 
