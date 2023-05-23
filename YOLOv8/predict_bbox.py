4fba67df7711ad4c6ba028d87f53ac539f90a807
eltonjohnfanboy <103358618+eltonjohnfanboy@users.noreply.github.com>
2023-05-23 17:51:34 +0200

    Yolo predictions checkpoints

--- YOLOv8/predict_bbox.py
+++ YOLOv8/predict_bbox.py
@@ -4,9 +4,11 @@ import matplotlib.pyplot as plt
 
 def predict_and_plot(image_path, model_path):
     # Load the model
-    model = YOLO(model_path)
+    #model = YOLO(model_path)
+    model = YOLO('yolov8n.yaml').load(model_path)
+    #model.conf = 0.1
     image = Image.open(image_path)
-    results = model(image)
+    results = model.predict(image, imgsz=1000, augment = True, conf=0.03, iou = 0.01)
 
     predictions = results[0].boxes.xyxy.numpy() # results[0].boxes.xyxyn.numpy()
 
@@ -23,6 +25,6 @@ def predict_and_plot(image_path, model_path):
     return predictions
 
 # Example usage
-image_path = 'C:/Users/adars/github-classroom/DCC-UAB/xnap-project-ed_group_01/YOLOv8/test/test_img/4.jpg'
-model_path = 'C:/Users/adars/github-classroom/DCC-UAB/xnap-project-ed_group_01/YOLOv8/yolov8n.pt'
+image_path = 'C:/Users/adars/github-classroom/DCC-UAB/xnap-project-ed_group_01/YOLOv8/test/test_img/1.jpg'
+model_path = 'C:/Users/adars/github-classroom/DCC-UAB/xnap-project-ed_group_01/YOLOv8/runs/last.pt'
 predict_and_plot(image_path, model_path)
