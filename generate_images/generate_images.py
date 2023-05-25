4d904b11bee497898fb026a2019db278b9e44673
eltonjohnfanboy <103358618+eltonjohnfanboy@users.noreply.github.com>
2023-05-25 22:46:23 +0200

    Noves fonts + color

--- generate_images/generate_images.py
+++ generate_images/generate_images.py
@@ -4,14 +4,17 @@ import numpy as np
 import sys
 import string
 import os
-sys.path.insert(0, '/home/alumne/ProjecteNN/xnap-project-ed_group_01')
+sys.path.insert(0, 'C:/Users/adars/github-classroom/DCC-UAB/xnap-project-ed_group_01')
 from YOLOv8.utils.utils import convert_bbox_to_yolo
 
 
-fonts = ["ARIAL.TTF", "CALIBRI.TTF", "COMIC.TTF", "CORBEL.TTF", "SEGOEPR.TTF"]
+fonts = ["ARIAL.TTF", "CALIBRI.TTF", "COMIC.TTF", "CORBEL.TTF", "SEGOEPR.TTF", "BERNHC.TTF", "COLONNA.TTF", "FRSCRIPT.TTF", "HARLOWSI.TTF", "INKFREE.TTF", "ITCBLKAD.TTF", "JOKERMAN.TTF", "MTCORSVA.TTF", "VINERITC.TTF"]
+text_colors = ["#000000", "#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF", "#00FFFF"]
+background_colors = [ "#F8F8F8", "#E5E5E5", "#D2D2D2", "#FFFFFF", "#F0F0F0", "#FAFAFA", "#EFEFEF", "#F5F5F5"]
 list_char = "123456789abcdefghijklmnopqrstuvwxyz"
 
 dict_char = {k:i for i,k in enumerate(string.ascii_lowercase + string.digits)}
+
 
 def generate_images(n, label_dir, images_dir, xy = (0,0)):
     for i in range(n):
@@ -22,13 +25,19 @@ def generate_images(n, label_dir, images_dir, xy = (0,0)):
         #with open(f"./data/labels/{i}.txt", "w") as file:
 
             font = fonts[random.randint(0, len(fonts)-1)]
-            font = ImageFont.truetype(f'/home/alumne/ProjecteNN/xnap-project-ed_group_01/generate_images/fonts/{font}', random.randint(15,30), encoding="unic")
+            
+            text_color = random.choice(text_colors)
+            background_color = random.choice(background_colors)
+            while text_color == background_color:
+                background_color = random.choice(background_colors)
+            
+            font = ImageFont.truetype(f'C:/Users/adars/github-classroom/DCC-UAB/xnap-project-ed_group_01/generate_images/fonts/{font}', random.randint(25,40), encoding="unic")
             _, _, w, h = font.getbbox(new_str)
-            size = (w + random.randint(2, 10), h + random.randint(2,10))
-            img = Image.new(mode="RGB", size=size, color='white')
+            size = (w + random.randint(10, 20), h + random.randint(10,20))
+            img = Image.new(mode="RGB", size=size, color= background_color)
 
             draw = ImageDraw.Draw(img)
-            draw.text(xy, new_str, font = font, fill = "black")
+            draw.text(xy, new_str, font = font, fill = text_color)
             for i, char in enumerate(new_str):
                 _, _, right, _ = font.getbbox(new_str[:i+1])
                 _, _, _, bottom = font.getbbox(char)
@@ -38,39 +47,14 @@ def generate_images(n, label_dir, images_dir, xy = (0,0)):
                 top = bottom - height #bbox[1]
                 left = right - width #bbox[0]
                 
-                #draw.rectangle((left, top, right, bottom), None, "#f00")
+                draw.rectangle((left, top, right, bottom), None, "#f00")
 
                 bbox_yolo = convert_bbox_to_yolo((left, top, width, height), size[0], size[1])
 
-                #char_index = 0 #comment for multiclass
-                char_index = dict_char[char]
+                char_index = 0 #comment for multiclass
+                #char_index = dict_char[char]
                 file.write(f"{char_index} {bbox_yolo[0]} {bbox_yolo[1]} {bbox_yolo[2]} {bbox_yolo[3]}\n")
             
-            #char_index = random.randint(0, len(list_char)-1)
-            #char = list_char[char_index]
-            #bbox = list(font.getbbox(char))
-            
-            
-            #width_box = bbox[2]-bbox[0]
-            #height_box = bbox[3]-bbox[1]
-            #x = random.randint(-bbox[0],size[0]-bbox[0]-width_box) 
-            #y = random.randint(-bbox[1],size[1]-bbox[1]-height_box)
-            #draw = ImageDraw.Draw(img)
-            #draw.text((x,y), char, font=font, fill = "black")
-
-
-
-            #bbox[0] =  max(0, bbox[0] +x)
-            #bbox[2] = min(size[0], bbox[2] +x)
-            #bbox[1] = max(0, bbox[1] +y)
-            #bbox[3] = min(size[1], bbox[3] +y)
-            
-            #draw.rectangle(bbox, None, "#f00")
-
-            #bbox[2] = bbox[2] - bbox[0]
-            #bbox[3] = bbox[3] - bbox[1]
-
-            #bbox_yolo = convert_bbox_to_yolo(bbox, size[0], size[1])
             file.close()
             img.save(os.path.join(images_dir, f"{new_str}.jpg"))
 
