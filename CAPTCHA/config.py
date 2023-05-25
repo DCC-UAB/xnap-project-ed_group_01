95e751616de7644b737bb2c4e7843ad46d0f3442
XavierQuerol <101579003+XavierQuerol@users.noreply.github.com>
2023-05-26 00:54:21 +0200

    new branch with captcha model

---
+++ CAPTCHA/config.py
@@ -0,0 +1,8 @@
+DATA_DIR = "C:/Users/xavid/Desktop/dataset/mjsynth/mnt/ramdisk/max/90kDICT32px/"
+ANNOTATIONS_FILE = "C:/Users/xavid/Documents/GitHub/xnap-project-ed_group_01/Datasets/annotation_val20.txt"
+BATCH_SIZE = 8
+IMAGE_WIDTH = 300
+IMAGE_HEIGHT = 75
+NUM_WORKERS = 8
+EPOCHS = 200
+DEVICE = "cpu"
