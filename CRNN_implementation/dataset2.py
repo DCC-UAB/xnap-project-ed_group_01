f766220e54b465ab03175c75d5543b1079caef89
eltonjohnfanboy <103358618+eltonjohnfanboy@users.noreply.github.com>
2023-05-28 13:00:45 +0200

    Small fixes

---
+++ CRNN_implementation/dataset2.py
@@ -0,0 +1,94 @@
+import albumentations
+import torch
+import os
+import numpy as np
+
+from scipy.io import loadmat
+
+from PIL import Image
+from PIL import ImageFile
+import cv2
+from torchvision import transforms
+from torch.utils.data import Dataset, DataLoader
+import string
+
+class Dataset(Dataset):
+
+    def __init__(self, mat_data, img_dir, type_data = "testdata", resize = (32,128)):
+        annots = loadmat(mat_data)
+
+        abspath = os.path.abspath(img_dir)
+        self.resize = resize
+        self.max_label_len = 0
+
+        self.img = []
+        self.orig_txt = []
+        self.label_length = []
+        self.input_length = []
+        self.txt_notpadded = []
+
+        for idx in range(annots[type_data][:,:].shape[1]):
+            path = annots[type_data][:,idx][0][0][0]
+            img = cv2.cvtColor(cv2.imread(os.path.join(abspath, path)), cv2.COLOR_BGR2GRAY) 
+
+            img = cv2.resize(img, (32, 128))
+            img = np.expand_dims(img , axis = 2)
+            img = np.transpose(img,(2, 1, 0))
+            img = img/255.
+
+            label = annots[type_data][:,idx][0][1][0]
+            self.img.append(img)
+            self.orig_txt.append(label)
+            self.input_length.append(31)
+            self.label_length.append(len(label))
+            self.txt_notpadded.append(self.encode_to_labels(label))
+
+            if len(label) > self.max_label_len:
+                self.max_label_len = len(label)
+        self.txt = self.pad_sequences(self.txt_notpadded, self.max_label_len, len(string.ascii_letters+string.digits))
+        # AMB VARIABLE LENGTH CAL FER PADDING THE SELF.TXT!!!!!!!!        
+
+    def pad_sequences(self, sequences, maxlen, padding_value):
+        padded_sequences = []
+        for sequence in sequences:
+            if len(sequence) >= maxlen:
+                padded_sequence = sequence[:maxlen]
+            else:
+                padded_sequence = sequence + [padding_value] * (maxlen - len(sequence))
+            padded_sequences.append(padded_sequence)
+        return padded_sequences
+
+    def encode_to_labels(self, txt):
+        # encoding each output word into digits
+        char_list = string.ascii_letters+string.digits
+        dig_lst = []
+        for index, char in enumerate(txt):
+            try:
+                dig_lst.append(char_list.index(char))
+            except:
+                print(char)
+        
+        return dig_lst
+
+    def __len__(self):
+        return len(self.img)
+    
+    def __getitem__(self, idx):
+        img_vector = torch.tensor(self.img[idx], dtype = torch.float32)
+        orig_txt = self.orig_txt[idx]
+        label_length_vect = torch.tensor(self.label_length[idx], dtype = torch.int32)
+        input_length_vector = torch.tensor(self.input_length[idx], dtype = torch.int32)
+        txt = torch.tensor(self.txt[idx], dtype = torch.long)
+
+        return img_vector, txt, orig_txt, label_length_vect, input_length_vector
+
+mat_data = "C:/Users/adars/OneDrive/Escritorio/ProjecteNN/IIIT5K/traindata.mat"
+img_dir = "C:/Users/adars/OneDrive/Escritorio/ProjecteNN/IIIT5K/"
+type_data = "traindata"
+aux = Dataset(mat_data, img_dir, type_data)
+#aux[0]
+
+#train_loader = DataLoader(
+#    aux, batch_size=16, shuffle=True
+#)
+#print("a")
