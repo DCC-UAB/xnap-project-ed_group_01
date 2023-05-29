2742414c67390996a91e36be32273c7cb64030fb
XavierQuerol <xqueroleso@gmail.com>
2023-05-29 16:08:53 +0000

    little changes

--- 28_05/Recognition/model.py
+++ 28_05/Recognition/model.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from torchvision.models import resnet18, vgg16, inception_v3
+from torchvision.models import resnet18, vgg11, inception_v3
 
 class CharacterClassifier(nn.Module):
     def __init__(self, num_classes, type_model = "resnet"):
@@ -11,7 +11,7 @@ class CharacterClassifier(nn.Module):
             num_features = self.model.fc.in_features
             self.model.fc = nn.Linear(num_features, num_classes)
         elif type_model == "vggnet":
-            self.model = vgg16(pretrained=True)
+            self.model = vgg11(pretrained=True)
             num_features = self.model.classifier[6].in_features
             self.model.classifier[6] = nn.Linear(num_features, num_classes)
         elif type_model == "inception":
