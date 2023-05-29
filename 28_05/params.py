c6ea69b37b28112fc5356ab0692cc9ffe1c5e67b
eltonjohnfanboy <adarsh0x1702@gmil.com>
2023-05-29 16:55:03 +0000

    Testing all pipelines

--- 28_05/params.py
+++ 28_05/params.py
@@ -1,7 +1,7 @@
 train_labels = "/home/alumne/Dataset/data_detection/random_chars/data/labels/train"
 train_images = "/home/alumne/Dataset/data_detection/random_chars/data/images/train"
-test_labels = "/home/alumne/Dataset/data_detection/random_chars/data/labels/test"
-test_images = "/home/alumne/Dataset/data_detection/random_chars/data/images/test"
+test_labels = "/home/alumne/ProjecteNN/data_detection/random_words/data/images/labels"
+test_images = "/home/alumne/ProjecteNN/data_detection/random_words/data/images/test"
 path_fonts = "/home/alumne/xnap-project-ed_group_01/28_05/generate_images/fonts"
 
 # YOLO
